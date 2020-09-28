a583f0cfed73cab7d06733cae9c89dd38bd5f91e
Dmytro Kardava <dima.kardava@gmail.com>
2020-09-28 23:04:06 +0200

    change course icon

--- app/MenuItems/Courses.php
+++ app/MenuItems/Courses.php
@@ -5,7 +5,7 @@ namespace App\MenuItems;
 class Courses extends AbstractMenuItem
 {
     public $href = 'admin.courses.index';
-    public $icon = 'fas fa-car';
+    public $icon = 'fas fa-paint-brush';
 
     public function __construct() {
         parent::__construct();
